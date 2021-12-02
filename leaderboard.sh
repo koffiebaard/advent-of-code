955470bd3bc156b6bf194d55c93aceda8fce88e6
Tim Quax <tim.quax@rabobank.nl>
2021-12-02 10:33:16 +0100

    check leaderboard from terminal

---
+++ leaderboard.sh
@@ -0,0 +1,3 @@
+data=$(curl -b "session=$AOC_SESSION_TOKEN" https://adventofcode.com/2021/leaderboard/private/view/$AOC_LEADERBOARD_ID.json -s)
+
+echo $data | jq -r '.members[] | "\(.local_score) \(.name)"' | sort -rn -k1.2 | sed 's/\([0-9]*\) \([a-zA-Z_]*\)/\2\ - \1/g'
