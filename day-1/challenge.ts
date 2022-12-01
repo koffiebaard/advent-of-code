d2a4946c4fae278ee6dd53bc9d9a6f6945169600
Tim Quax <tim.quax@rabobank.nl>
2022-12-01 10:40:17 +0100

    Cleaned up some more

--- day-1/challenge.ts
+++ day-1/challenge.ts
@@ -2,17 +2,21 @@ import { readFileSync } from 'fs';
 
 const input = readFileSync('./input', 'utf-8');
 
-const calories_carried_by_elves = input.split(/\n\n/)
+const calories_carried_by_elves = input
+  .split(/\n\n/)
   .map(elf_calories => {
     return elf_calories
       .split(/\n/)
       .map(calories => parseInt(calories) || 0)
       .reduce((total, calories) => total + calories, 0);
-  })
+  });
 
-let highest_calories = calories_carried_by_elves
+let highest_calories_one_elf = Math.max(...calories_carried_by_elves);
+
+let highest_calories_three_elves = calories_carried_by_elves
   .sort((n1, n2) => n2 - n1)
   .slice(0, 3)
   .reduce((total, calories) => total + calories, 0);
 
-console.log(highest_calories);
+console.log(highest_calories_one_elf);
+console.log(highest_calories_three_elves);
