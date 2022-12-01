682c33b1e02603bd53deeb8c1fa7af652f923a0d
Tim Quax <tim.quax@rabobank.nl>
2022-12-01 10:13:37 +0100

    Made day 1 a bit nicer

--- day-1/chal1.ts
+++ day-1/chal1.ts
@@ -2,13 +2,14 @@ import { readFileSync } from 'fs';
 
 const input = readFileSync('./input', 'utf-8');
 
-const calorie_chunks = input.split(/\n\n/)
-  .map(chunk => { 
-    return chunk
+const calories_carried_by_elves = input.split(/\n\n/)
+  .map(elf_calories => {
+    return elf_calories
       .split(/\n/)
-      .map(calories => parseInt(calories))
-      .reduce((total,calories) => total + calories, 0);
-  })
-  .filter(calorie_chunk => !isNaN(calorie_chunk));
+      .map(calories => parseInt(calories) || 0)
+      .reduce((total, calories) => total + calories, 0);
+  });
 
-console.log(Math.max(...calorie_chunks));
+let highest_calories = Math.max(...calories_carried_by_elves);
+
+console.log(highest_calories);
--- day-1/chal2.ts
+++ day-1/chal2.ts
@@ -2,17 +2,17 @@ import { readFileSync } from 'fs';
 
 const input = readFileSync('./input', 'utf-8');
 
-const calorie_chunks = input.split(/\n\n/)
-  .map(chunk => {
-    return chunk
+const calories_carried_by_elves = input.split(/\n\n/)
+  .map(elf_calories => {
+    return elf_calories
       .split(/\n/)
       .map(calories => parseInt(calories) || 0)
-      .reduce((total,calories) => total + calories, 0);
+      .reduce((total, calories) => total + calories, 0);
   })
 
-let calorie_amount = calorie_chunks
+let highest_calories = calories_carried_by_elves
   .sort((n1, n2) => n2 - n1)
   .slice(0, 3)
-  .reduce((total,calories) => total + calories, 0);
+  .reduce((total, calories) => total + calories, 0);
 
-console.log(calorie_amount);
+console.log(highest_calories);
