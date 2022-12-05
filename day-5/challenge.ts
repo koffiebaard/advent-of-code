0771fef658a037fd253a87b4674bd30470cc76bc
Koffiebaard <tim@koffiemok.net>
2022-12-05 13:16:50 +0100

    prettify()

--- day-5/challenge.ts
+++ day-5/challenge.ts
@@ -7,12 +7,13 @@ input.split(/\n\n/)[0]
   .split(/\n/)
   .filter(line => line !== '' && line.trim().substring(0, 1) === '[' )
   .map(line => {
-    for (let i = 1, stack_number = 0; i < line.length; i = i + 4, stack_number++) {
-      let crate = line.substring(i, i + 1);
-
-      stacks[stack_number] = stacks[stack_number] || [];
-      crate.trim() !== '' && stacks[stack_number].push(crate);
-    }
+    line.slice(1).split('').reduce((stacks: string[][], crate, i) => {
+      if (i % 4 === 0 && crate !== ' ') {
+        stacks[i / 4] = stacks[i / 4] || [];
+        stacks[i / 4].push(crate);
+      }
+      return stacks;
+    }, stacks);
   });
 
 let stacks_2: string[][] = JSON.parse(JSON.stringify(stacks));
@@ -21,24 +22,19 @@ input.split(/\n\n/)[1]
   .split(/\n/)
   .filter(line => line !== '')
   .map(line => {
-    let [amount, from, to] = line
-      .replace(/move ([0-9]+) from ([0-9]+) to ([0-9]+)/, '$1 $2 $3')
-      .split(' ')
-      .map(item => parseInt(item));
+    let [amount, from, to] = (line.match(/\d+/g) || []).map(move => parseInt(move));
 
     // Challenge 1
-    for (let i = 0; i < amount; i++) {
-      let crate = stacks[from-1].shift();
-      stacks[to-1].unshift(String(crate));
-    }
+    let crates = stacks[from-1].slice(0, amount);
+    stacks[from-1] = stacks[from-1].slice(amount);
+
+    stacks[to-1].unshift(...crates.reverse());
 
     // Challenge 2
-    let crates = stacks_2[from-1].slice(0, amount);
+    crates = stacks_2[from-1].slice(0, amount);
     stacks_2[from-1] = stacks_2[from-1].slice(amount);
 
-    while (crates.length > 0) {
-      stacks_2[to-1].unshift(String(crates.pop()));
-    }
+    stacks_2[to-1].unshift(...crates);
   });
 
 console.log(stacks.map(stack => stack[0]).join(''));
