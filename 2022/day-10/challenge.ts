8c85e62e16f028bb07323a62c894d63481c79448
Koffiebaard <tim@koffiemok.net>
2022-12-10 12:37:11 +0100

    Cleaned up day 10 a bit

--- 2022/day-10/challenge.ts
+++ 2022/day-10/challenge.ts
@@ -4,16 +4,16 @@ const input = readFileSync(process.argv[2] ?? './input', 'utf-8');
 
 let cycles = 0;
 let x = 1;
-var signal_strengths: {[key: number]: number} = {};
+var signal_strength_sum = 0;
 var position = 0;
 
-function write_signal_strength(cycles: number) {
+function write_signal_strength(cycles: number, x: number) {
   if (cycles === 20 || (cycles-20) % 40 === 0)
-      signal_strengths[cycles] = x;
+    signal_strength_sum += cycles * x;
 }
 
-function draw_pixel(cycles: number, x: number) {
-  let sprite_start = x - 2, sprite_end = x;
+function draw_pixel(x: number) {
+  let sprite_start = x - 1, sprite_end = x + 1;
 
   if (position >= sprite_start && position <= sprite_end)
     process.stdout.write('#');
@@ -22,7 +22,7 @@ function draw_pixel(cycles: number, x: number) {
 
   position++;
 
-  if (cycles % 40 === 0) {
+  if (position === 40) {
     position = 0;
     process.stdout.write('\n');
   }
@@ -33,25 +33,18 @@ input
   .filter(output => output !== '')
   .map(instruction => {
     cycles++;
-
-    write_signal_strength(cycles);
-    draw_pixel(cycles, x); 
+    write_signal_strength(cycles, x);
+    draw_pixel(x); 
 
     if (instruction === 'noop')
       return;
 
     cycles++;
-   
-    write_signal_strength(cycles);
+    write_signal_strength(cycles, x);
+    draw_pixel(x);
 
     let addx = Number(instruction.split(' ')[1]);
     x += addx;
-
-    draw_pixel(cycles, x);
   });
 
-let sum_signal_strengths = Object.keys(signal_strengths).reduce((total, value) => {
-  return total + Number(value) * signal_strengths[Number(value)];
-}, 0);
-
-console.log(sum_signal_strengths);
+console.log(signal_strength_sum);
