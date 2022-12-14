b2c606cf1458a33fb567515d85a902e918e4f1d0
Koffiebaard <tim@koffiemok.net>
2022-12-14 07:54:30 +0100

    ascii art for day 13

--- 2022/day-13/challenge.ts
+++ 2022/day-13/challenge.ts
@@ -28,28 +28,52 @@ function compare_values(left: number | number[], right: number | number[]): bool
   return result;
 }
 
-let packets = input.split('\n\n').filter(output => output !== '')
+// Challenge 1
+let packets = input
+  .split('\n\n')
+  .filter(output => output !== '')
+  .map(line => ({
+    left: JSON.parse(line.split('\n')[0]),
+    right: JSON.parse(line.split('\n')[1])
+  }));
 
-let pairs_in_right_order = packets.reduce((indices, line, index) => {
-    let left = JSON.parse(line.split('\n')[0]);
-    let right = JSON.parse(line.split('\n')[1]);
+let pairs_in_right_order = packets.reduce((indices, pair, index) => {
+  return compare_values(pair.left, pair.right) ? indices + index + 1 : indices
+}, 0);
 
-    let result = compare_values(left, right);
+// Challenge 2
+let all_packets = input
+  .split('\n')
+  .filter(output => output !== '')
+  .concat('[[2]]', '[[6]]')
+  .sort((left, right) => {
+    return compare_values(JSON.parse(left), JSON.parse(right)) ? -1 : 1
+  });
 
-    return result ? indices + index + 1 : indices;
-  }, 0);
+let divider_packet_indices = (all_packets.indexOf('[[2]]') + 1) * (all_packets.indexOf('[[6]]') + 1);
 
-let all_packets = input.split('\n').filter(output => output !== '').concat('[[2]]', '[[6]]');
 
-all_packets.sort((line1, line2) => {
-  let left = JSON.parse(line1);
-  let right = JSON.parse(line2);
-
-  let result = compare_values(left, right);
-
-  return result ? -1 : 1;
-})
-
-console.log(pairs_in_right_order);
-
-console.log((all_packets.indexOf('[[2]]') + 1) * (all_packets.indexOf('[[6]]') + 1));
+let red = '38;2;255;0;0';
+let green = '38;2;0;208;0';
+let dark_green = '38;2;0;90;0';
+let rs = `\x1b[${red}m`;
+let gs = `\x1b[${green}m`;
+let dgs = `\x1b[${dark_green}m`;
+let ws = `\x1b[97m`;
+let e = `\x1b[0m`;
+console.log(`${ws}                                   .
+      .              .   .'.     \\   /
+    \\   /      .'. .' '.'   ' -= ${rs}${String(divider_packet_indices).padStart(4).padEnd(5)}${ws} =-
+  -= ${rs}${String(pairs_in_right_order).padStart(3).padEnd(4)}${ws} =-  .'   '             / | \\
+    / | \\${gs}                          |
+      |                            |
+      |                            |
+      |                      .=====|
+      |=====.                |${dgs}.---.${gs}|
+      |${dgs}.---.${gs}|                |${dgs}|=o=|${gs}|
+      |${dgs}|=o=|${gs}|                |${dgs}|   |${gs}|
+      |${dgs}|   |${gs}|                |${dgs}|   |${gs}|
+      |${dgs}|   |${gs}|                |${dgs}|___|${gs}|
+      |${dgs}|___|${gs}|                |${dgs}[${gs}:::${dgs}]${gs}|
+      |${dgs}[${gs}:::${dgs}]${gs}|                '-----'
+      '-----'${e}`);
